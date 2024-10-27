5ceb559cbe93480880a3373447e35cf597561219
cdev <112781737+connor-c19@users.noreply.github.com>
2024-10-27 18:14:59 -0400

    Add LLM Mealplan Generation

--- example_config.py
+++ example_config.py
@@ -2,12 +2,12 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-MYSQL_USERNAME = ''
+MYSQL_USERNAME = 'root'
 MYSQL_PASSWORD = ''
 MYSQL_HOST = 'localhost'
 MYSQL_DB = 'shapeshift'
 
 class Config:
-    SECRET_KEY = 'secret-key'  
+    SECRET_KEY = 'xxx'  
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
